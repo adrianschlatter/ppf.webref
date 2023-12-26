62e9f9bcbe2326d4608f0c9221d75dcc36f3fa72
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2023-12-26 22:44:10 +0100

    fix sqlalchemy deprecation warning

--- src/ppf/webref/__init__.py
+++ src/ppf/webref/__init__.py
@@ -71,7 +71,7 @@ def create_app(test=False):
 
     @login_manager.user_loader
     def load_user(user_id):
-        return User.query.get(int(user_id))
+        return db.session.get(User, int(user_id))
 
     @app.route('/')
     @login_required
