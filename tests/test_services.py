25cc1eeb78b2eedaf279f20669ece9ba60cc6aac
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2023-12-29 21:44:13 +0100

    test /loadEntries.php
    * extend db model so I am able to insert test data into the db
    * test whether we can retrieve the test data via a simple search

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,4 +1,5 @@
 import pytest
+from ppf.webref.model import Entry, db
 
 
 @pytest.fixture()
@@ -6,6 +7,26 @@ def client_logged_in(app):
     client = app.test_client()
     client.post('login',
                 data={'username': 'existing_user', 'password': 'password'})
+
+    entry = Entry(type='article', version=1,
+                  fields={
+                      'author': 'Åström, Karl Johan and Murray, Richard M.',
+                      'title': 'Feedback Systems',
+                      'publisher': 'Princeton University Press',
+                      'year': '2020',
+                      'citationkey': 'a',
+                      'month': '#jul#',
+                      'url': ('http://www.cds.caltech.edu/~murray/books/AM08/'
+                              'pdf/fbs-public_24Jul2020.pdf'),
+                      'file': ':a.pdf:PDF',
+                      'keywords': ('feedback, systems, continuous, control, '
+                                   'stability, modeling, dynamics, '
+                                   'linear systems, state space, t')})
+
+    with app.app_context():
+        db.session.add(entry)
+        db.session.commit()
+
     return client
 
 
@@ -17,3 +38,13 @@ def test_index(client_logged_in):
 def test_references(client_logged_in):
     response = client_logged_in.get('/references/does_not_exist.pdf')
     assert response.status_code == 404
+
+
+def test_loadEntries(client_logged_in):
+    with client_logged_in as client:
+        response = client.post('loadEntries.php',
+                               data={'searchexpr': 'Feedback'})
+
+    assert '<table>' in response.text
+    assert '</table>' in response.text
+    assert '<td>Feedback Systems</td>' in response.text
